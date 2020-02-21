e8a931be2af46a2b1df246099b6a84a7983e5996
Samy Dindane <samy@dindane.com>
2020-02-21 20:43:14 +0100

    Set shift to one of the thumb keys

--- keyboards/kyria/keymaps/dinduks/keymap.c
+++ keyboards/kyria/keymaps/dinduks/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | Alt  | GUI  | Enter|      |      |  |      |      | Space| Bksp | AltGr|
+ *                        | Alt  | GUI  | Enter|LShift|      |  |      |LShift| Space| Bksp | AltGr|
  *                        |      |      | Lower|      |      |  |      |      | Raise| Symb |      |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,                  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_BSLS_GRV),
     MT(MOD_LCTL,KC_ESC),     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                      KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_SCLN_COLN), KC_QUOT,
     KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,  _______, _______, _______, _______, KC_N,    KC_M,    TD(TD_COMM_CCEDILLE), KC_DOT,  KC_SLSH, MT(MOD_RSFT,KC_CAPS),
-             KC_LALT, KC_LGUI, LT(_LOWER,KC_ENT), _______, _______, _______, _______, LT(_RAISE,KC_SPC), LT(_SYMBOLS, KC_BSPC), KC_RALT
+             KC_LALT, KC_LGUI, LT(_LOWER,KC_ENT), KC_LSFT, _______, _______, KC_LSFT, LT(_RAISE,KC_SPC), LT(_SYMBOLS, KC_BSPC), KC_RALT
   ),
 
 /*
